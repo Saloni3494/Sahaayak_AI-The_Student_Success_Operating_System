b4c1b31b8849b9e3d2b84c0e8805769ef974e35f
Prathamesh Nawale <prathameshnawale19@gmail.com>
2026-06-28 02:18:39 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,6 +43,7 @@ if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.BACKEND_CORS_ORIGINS,
+        allow_origin_regex=r"https://.*\.vercel\.app",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
